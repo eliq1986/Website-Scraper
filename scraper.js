92c1a710a3828ee6cf8e74e0d9c962d421f59716
Elijah Quesada <this_boy10@yahoo.com>
2018-07-27 14:12:06 -0700

    =Scraped website

--- scraper.js
+++ scraper.js
@@ -47,20 +47,44 @@ const result  = fs.existsSync(`${hardCoded.folder}`);
         });
     }
 
-    const arrayOfShirts = [];
+
+
+  const arrayOfShirts = [];
     arrayOfPricesLinks.forEach(link => {
-      const shirt = {};
+
+    const shirtObj = {};
       axios.get(`http://shirts4mike.com/${link}`)
       .then((response) => {
           const $ = cheerio.load(response.data);
           let price, name;
 
+         let imageSrc = $(".shirt-picture img").attr("src");
+         imageSrc = `http://www.shirts4mike.com/${imageSrc}`
           let shirtDetails = $(".shirt-details h1").text();
+            const url = response.config.url;
           [price, ...name] = shirtDetails.split(" ");
           name = name.join(" ");
           name = name.split(",")
-          shirt.title = name[0];
-          shirt.price = price;
+          shirtObj.title = name[0];
+          shirtObj.price = price;
+          shirtObj.imageSource = imageSrc;
+          shirtObj.url = url;
+
+          //
+          //
+          // console.log("Date: ",response.status);
+          //         console.log("Config: ",response.config);
+          //
+
+
+
+          arrayOfShirts.push(shirtObj);
+
+
+          return arrayOfShirts;
+
+      }).then((array)=> {
+       console.log(array[array.length - 1])
 
       }).catch((error)=> {
           console.log(error)
@@ -68,6 +92,5 @@ const result  = fs.existsSync(`${hardCoded.folder}`);
 
 
     })
-
 
 });
