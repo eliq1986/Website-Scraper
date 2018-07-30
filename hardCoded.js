52f7b2f1b5f65cf620a4e5ab4f17560ae972e5b7
Elijah Quesada <this_boy10@yahoo.com>
2018-07-30 11:12:32 -0700

    Accessed image links via axios

--- hardCoded.js
+++
@@ -1,7 +0,0 @@
-const folder = "./data";
-const url = `http://shirts4mike.com/shirts.php`;
-
-module.exports = {
-      folder,
-       url
-     };
--- scraper.js
+++ scraper.js
@@ -2,95 +2,103 @@
 
 const command = process.argv[2];
 
-
-
-// request data string from module
-const hardCoded = require("./hardCoded.js");
+const entryURL = "http://shirts4mike.com/shirts.php";
 
 
 // loads node core modules
 const fs = require("fs");
-const axios = require("axios");
+
 
 
 
 //loads npm modules
-
 const request = require("request"),
       cheerio = require("cheerio"),
-      mkdir = require("mkdirp");
+      mkdir = require("mkdirp"),
+      axios = require("axios");
 
 
 
 
 // checks if folder exists
-const result  = fs.existsSync(`${hardCoded.folder}`);
-      result ? null : mkdir(`${hardCoded.folder}`);
+const result  = fs.existsSync(`./data`);
+      result ? null : mkdir(`./data`);
 
 
 
 //makes request to url
+axios.get(entryURL).then((response) => {
+     let arrayOfLinks = [];
 
-    request(hardCoded.url, function(error,response, body) {
-   let arrayOfPricesLinks = [];
-    if (!error && response.statusCode === 200) {
+     const $ = cheerio.load(response.data);
+     const ulList = $("ul.products a");
 
-        const $ = cheerio.load(body);
+          ulList.each(function(i,elem)  {
+            arrayOfLinks.push(elem.attribs.href);
 
-        const ulList = $("ul.products  a");
+          });
 
+     const url = response.config.url;
+     arrayOfLinks.push(url);
+     return arrayOfLinks;
+}).then((res)=>{
+   let arrayOfObj = [];
+   let url = res.pop();
+    const newURL = url.slice(0, 23);
+     res.forEach(link => {
+        request(`${newURL}${link}`, (error, response, body) => {
+        // console.log(response.statusCode);
+          const $ = cheerio.load(body);
+             //console.log(body);
+             let imageSrc = $(".shirt-picture img").attr("src");
+            console.log(imageSrc);
 
-
-        ulList.each(function(i,elem)  {
-          arrayOfPricesLinks.push(elem.attribs.href);
-
-        });
-    }
-
-
-
-  const arrayOfShirts = [];
-    arrayOfPricesLinks.forEach(link => {
-
-    const shirtObj = {};
-      axios.get(`http://shirts4mike.com/${link}`)
-      .then((response) => {
-          const $ = cheerio.load(response.data);
-          let price, name;
-
-         let imageSrc = $(".shirt-picture img").attr("src");
-         imageSrc = `http://www.shirts4mike.com/${imageSrc}`
-          let shirtDetails = $(".shirt-details h1").text();
-            const url = response.config.url;
-          [price, ...name] = shirtDetails.split(" ");
-          name = name.join(" ");
-          name = name.split(",")
-          shirtObj.title = name[0];
-          shirtObj.price = price;
-          shirtObj.imageSource = imageSrc;
-          shirtObj.url = url;
-
-          //
-          //
-          // console.log("Date: ",response.status);
-          //         console.log("Config: ",response.config);
-          //
-
-
-
-          arrayOfShirts.push(shirtObj);
-
-
-          return arrayOfShirts;
-
-      }).then((array)=> {
-       console.log(array[array.length - 1])
-
-      }).catch((error)=> {
-          console.log(error)
-      })
-
-
-    })
-
+       })
+     })
+   setTimeout(()=> {
+//    console.log(arrayOfObj);
+  }, );
+}).catch((err) => {
+  console.log(err);
 });
+
+
+
+
+//   const arrayOfShirts = [];
+//     arrayOfPricesLinks.forEach(link => {
+//
+//     const shirtObj = {};
+//       axios.get(`http://shirts4mike.com/${link}`)
+//       .then((response) => {
+//           const $ = cheerio.load(response.data);
+//           let price, name;
+//
+//          let imageSrc = $(".shirt-picture img").attr("src");
+//          imageSrc = `http://www.shirts4mike.com/${imageSrc}`
+//           let shirtDetails = $(".shirt-details h1").text();
+//             const url = response.config.url;
+//           [price, ...name] = shirtDetails.split(" ");
+//           name = name.join(" ");
+//           name = name.split(",")
+//           shirtObj.title = name[0];
+//           shirtObj.price = price;
+//           shirtObj.imageSource = imageSrc;
+//           shirtObj.url = url;
+//
+//           arrayOfShirts.push(shirtObj);
+//
+//
+//           return arrayOfShirts;
+//
+//       }).then((array)=> {
+//        console.log(array[array.length - 1])
+//
+//       }).catch((error)=> {
+//           console.log(error)
+//       })
+//
+//
+//     })
+//
+// });
