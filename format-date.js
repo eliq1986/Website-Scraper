45b60be202067855a4d499f9b9c9ee691f2efe69
Elijah Quesada <this_boy10@yahoo.com>
2018-08-01 10:42:15 -0700

    Added request-promises

--- format-date.js
+++ format-date.js
@@ -1,15 +1,33 @@
-
-
 const formatDate = (date) => {
-     let day, year, formatDate, dateArr;
-    date = date.slice(5, 16);
-     dateArr = date.split(" ");
-     day = dateArr.shift();
-     year = dateArr.pop();
-     formatDate = `${year}-${dateArr[0]}-${day}`;
-     
-     return formatDate;
-}
+  let day, year, formatDate, dateArr;
+  date = date.slice(5, 16);
+   dateArr = date.split(" ");
+   day = dateArr.shift();
+   year = dateArr.pop();
 
+
+  const monthNumber = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec"
+  ].indexOf(dateArr[0]) + 1;
+
+
+  if (monthNumber < 10) {
+    return `${year}-0${monthNumber}-${day}`;
+  } else {
+    return `${year}-1${monthNumber}-${day}`;
+  }
+
+}
 
 module.exports.formatDate = formatDate;
--- scraper.js
+++ scraper.js
@@ -1,127 +1,98 @@
 "use strict";
 
+const webSiteURL = "http://shirts4mike.com";
 const entryURL = "http://shirts4mike.com/shirts.php";
 
 // loads node core modules
 const fs = require("fs");
 
-//loads custom modules
-const formatDate = require("./format-date.js");
-const formatTime = require("./format-time.js");
-
-
 //loads npm modules
 const request = require("request"),
-      cheerio = require("cheerio"),
-      mkdir = require("mkdirp"),
-      axios = require("axios"),
-      jsonexport = require('jsonexport');
-
+  cheerio = require("cheerio"),
+  mkdir = require("mkdirp"),
+  rp = require("request-promise"),
+  jsonexport = require('jsonexport');
 
 // checks if folder exists
-const result  = fs.existsSync(`./data`);
-      result ? null : mkdir(`./data`);
+  const result = fs.existsSync(`./data`);
+  result ? null: mkdir(`./data`);
+
+// loads custom modules
+  const date = require("./format-date.js");
+  const time = require("./format-time.js");
 
 
 
-//makes request to url
- request("http://shirts4mike.com", (error, response, body)=> {
-     if (!error) {
+// makes request to url
+request(`${webSiteURL}`, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+     const url = response.request.uri.href;
+    rp(`${entryURL}`).then((body) => {
+      let arr = [];
+      const dateResponse = response.headers.date;
+      const $ = cheerio.load(body);
+      const ulList = $("ul.products a");
 
-axios.get(entryURL).then((response) => {
-     let arrayOfLinks = [];
+      ulList.each(function(i, elem) {
+        arr.push(elem.attribs.href);
+      });
+      const formattedDate = date.formatDate(dateResponse);
+      const formattedTime = time.formatTime(dateResponse);
+      arr.push(formattedTime)
+      arr.push(formattedDate);
+      arr.push(url);
+      return arr;
 
-     const $ = cheerio.load(response.data);
-     const ulList = $("ul.products a");
-
-
-
-          ulList.each(function(i,elem)  {
-            arrayOfLinks.push(elem.attribs.href);
-
-          });
-
-
-          const date = response.headers.date;
-
-
-
-          const formattedDate = formatDate.formatDate(date);
-          const formattedTime = formatTime.formatTime(date);
-
-
-
-     const url = response.config.url;
-     arrayOfLinks.push(url, formattedDate, formattedTime);
-     return arrayOfLinks;
+    }).then((arr)=> {
+   //console.log(arr);
+      let arrayOfObj = [];
+  const url = arr.pop();
+  const formattedDate = arr.pop();
+  const formattedTime = arr.pop();
 
 
-}).then((res)=>{
+
+//  const newURL = url.slice(0, 23);
 
 
-   let arrayOfObj = [];
-   const formattedTime = res.pop();
-   const formattedDate = res.pop();
-   const url = res.pop();
-
-
-    const newURL = url.slice(0, 23);
-
-     res.forEach(link => {
-        const shirtObj = {};
+  arr.forEach((link) => {
+      const shirtObj = {};
       let price, name, title, imageURL, shirtDetails, shirtURL;
-        axios.get(`${newURL}${link}`).then((response) => {
+      request(`${url}${link}`, (error, response, body) => {
+
+     const $ = cheerio.load(body);
+       shirtURL = response.request.uri.href;
+
+    imageURL = $(".shirt-picture img").attr("src");
+    shirtDetails = $(".shirt-details h1").text();
+
+    [price, ...name] = shirtDetails.split(" ");
+     name = name.join(" ");
+     name = name.split(",");
+     shirtObj.Title = name[0];
+     shirtObj.Price = price;
+     shirtObj.ImageURL = imageURL;
+     shirtObj.URL = shirtURL;
+     shirtObj.Time = formattedTime;
+
+
+     arrayOfObj.push(shirtObj)
+
+      })
 
 
 
-              const $ = cheerio.load(response.data);
-                shirtURL = response.config.url;
+  })
+  setTimeout(()=> {
 
-             imageURL = $(".shirt-picture img").attr("src");
-             shirtDetails = $(".shirt-details h1").text();
-
-             [price, ...name] = shirtDetails.split(" ");
-              name = name.join(" ");
-              name = name.split(",");
-              shirtObj.Title = name[0];
-              shirtObj.Price = price;
-              shirtObj.ImageURL = imageURL;
-              shirtObj.URL = shirtURL;
-              shirtObj.Time = formattedTime;
-
-
-              arrayOfObj.push(shirtObj);
-
-
-        const date = response.headers.date;
-
-       })
-
-     })
-     setTimeout(()=> {
-        const jsonArrayOfObjects = arrayOfObj;
-
-        jsonexport( jsonArrayOfObjects,function(err, csv){
-            if(err) return console.log(err);
-            fs.writeFileSync(`./data/${formattedDate}.csv`, csv);
-        });
-}, 2000);
-
-
-}).catch((err) => {
-  console.log(err);
+             jsonexport(arrayOfObj, function(err, csv) {
+            if (err)
+              return console.log(err);
+              fs.writeFileSync(`./data/${formattedDate}.csv`, csv);
+            });
+  },1000);
 });
-  } else {
-    const date = new Date;
-    const formatErrorDate = (date) => {
-
-    const yearMonthDay = date.toString();
-     const message = `[${yearMonthDay}] There's been a 404 error <br />`;
-    fs.writeFileSync("./scraper-error.log", message);
-    }
-    formatErrorDate(date);
-
-
 }
+});
 
-})
+/
